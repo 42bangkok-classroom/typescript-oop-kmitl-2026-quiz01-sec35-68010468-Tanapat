d86e301e4d0c0b5df7bae4d3ef171625907ff920
68010468-Tanapat <68010468@kmitl.ac.th>
2025-12-15 09:58:14 +0700

    2

--- src/q02/atm.ts
+++ src/q02/atm.ts
@@ -1,12 +1,12 @@
-const withdrawal: number = parseInt(process.argv[2])
-const balance: number = parseInt(process.argv[3])
+const balance: number = parseInt(process.argv[2])
+const withdrawal: number = parseInt(process.argv[3])
 if(Number.isNaN(withdrawal) || Number.isNaN(balance)){
     console.log('Invalid Input')
 }
 else if( withdrawal > 5000){
     console.log('Exceeds per-withdrawal limit')
 }
-else if(withdrawal < balance){
+else if(withdrawal > balance){
     console.log('Insufficient balance')
 }
 else{
